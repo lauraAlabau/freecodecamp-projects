9c95c59e20e4347c4fd78a039329b7357ee43d92
lauraAlabau <arlaura3@gmail.com>
2021-12-07 12:59:57 +0100

    Navbar added

--- 15-react-projects/13-stripe-submenus/src/components/Hero/Hero.js
+++ 15-react-projects/13-stripe-submenus/src/components/Hero/Hero.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import { useGlobalContext } from '../../contexts/context'
+import React from "react";
+import { useGlobalContext } from "../../contexts/context";
 
-import phoneImg from '../../images/phone.svg'
+import phoneImg from "../../images/phone.svg";
 
 const Hero = () => {
+  const {closeSubmenu} = useGlobalContext();
+  return <h2>hero component</h2>;
+};
 
-  const data = useGlobalContext()
-  console.log(data)
-  return <h2>hero component</h2>
-}
-
-export default Hero
+export default Hero;
