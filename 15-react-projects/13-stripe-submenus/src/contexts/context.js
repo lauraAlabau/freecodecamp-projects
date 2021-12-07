bd9386d6daee5f3db6cc373e9d886ee456478990
lauraAlabau <arlaura3@gmail.com>
2021-12-07 15:26:48 +0100

    stripe submenu and location

--- 15-react-projects/13-stripe-submenus/src/contexts/context.js
+++ 15-react-projects/13-stripe-submenus/src/contexts/context.js
@@ -5,7 +5,8 @@ const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({})
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -14,7 +15,8 @@ export const AppProvider = ({ children }) => {
     setIsSidebarOpen(false);
   };
 
-  const openSubmenu = () => {
+  const openSubmenu = (text, coordinates) => {
+    setLocation(coordinates)
     setIsSubmenuOpen(true);
   };
   const closeSubmenu = () => {
@@ -30,6 +32,7 @@ export const AppProvider = ({ children }) => {
         openSidebar,
         closeSubmenu,
         closeSidebar,
+        location,
       }}
     >
       {children}
